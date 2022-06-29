a17870aa5fd319c37b76862ddc82f393a550bdeb
村上信次 <smurakami@morisawa.co.jp>
2022-06-29 09:36:34 +0900

    first commit

--- myapp2/src/App.jsx
+++ myapp2/src/App.jsx
@@ -2,11 +2,19 @@ import React, { useState } from 'react';
 import Header from './modules/Header';
 import Form from './modules/Form';
 import List from './modules/List';
+import Export from './modules/Export';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AddUser from './modules/AddUser';
+import { useAuth0 } from "@auth0/auth0-react";
 
 
 export default function App() {
+  const RedirectToLogin = () => {
+    const { loginWithRedirect } = useAuth0();
+    loginWithRedirect();
+    return <></>;
+  };
+  const { user, isAuthenticated, isLoading, logout } = useAuth0();
   const [flag, setFlag] = useState(false);
   const [value, setValue] = useState('');
   const [display, setDisplay] = useState({
@@ -23,17 +31,44 @@ export default function App() {
 
   return (
     <>
-      <Router>
-        {/* ヘッダー */}
-        <Header />
-        {/* フォーム */}
-        <Form display={display} setDisplay={setDisplay} setFlag={setFlag} setValue={setValue} />
-        {/* テーブル(検索結果の表示) */}
-        <List searchResult={display} displayFlag={flag} postValue={value} />
-        <Routes>
-          <Route path="/adduser" element={<AddUser />} />
-        </Routes>
-      </Router>
+      {isAuthenticated ? (
+        // 認証済み時はログイン後の画面を表示
+        <>
+          <Router>
+            {/* ヘッダー */}
+            <Header />
+            {/* フォーム */}
+            <Form display={display} setDisplay={setDisplay} setFlag={setFlag} setValue={setValue} />
+            {/* テーブル(検索結果の表示) */}
+            <List searchResult={display} displayFlag={flag} postValue={value} />
+            <Export postValue={value} />
+            <Routes>
+              <Route path="/adduser" element={<AddUser />} />
+            </Routes>
+            <div>
+                <img src={user.picture} alt={user.name} />
+                <h6>{user.name}({user.email})</h6>
+                <button
+                  onClick={() => {
+                    logout({ returnTo: window.location.origin });
+                  }}
+                >
+                Log out
+                </button>
+              </div>
+          </Router>
+        </>
+      ) : isLoading ? (
+        // ログイン試行中はローディング画面を表示
+        <>
+          <div>Loading...</div>
+        </>
+      ) : (
+        // 未認証時はAuth0のログイン画面にリダイレクト
+        <>
+          <RedirectToLogin />
+        </>
+      )}
     </>
   )
 }
--- myapp2/src/index.js
+++ myapp2/src/index.js
@@ -1,9 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+    <Auth0Provider
+      domain={process.env.REACT_APP_AUTHDOMAIN}
+      clientId={process.env.REACT_APP_AUTHCLIENTID}
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>,
   document.getElementById("root")
 );
